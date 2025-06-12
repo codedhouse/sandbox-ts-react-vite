cc28df448f25b9603921e1f9e650aadddb31f602
Vivien <vivien-public@pm.me>
2025-06-11 22:41:00 -0400

    Update FeedbackItem component

--- feedback-app/src/components/FeedbackItem.tsx
+++ feedback-app/src/components/FeedbackItem.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
+
 function FeedbackItem() {
+  // Set default rating to 7
+  const [rating, setRating] = useState(7)
+  const [feedback, setText] = useState('This is an example feedback item.')
+
   return (
     <div className="card">
-      <div className="num-display">10</div>
-      <div className="text-display">This is an example feedback item.</div>
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{feedback}</div>
     </div>
   )
 }
