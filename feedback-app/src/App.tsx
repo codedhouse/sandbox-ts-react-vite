442e8df86cfa8ad92d73fed21befc8a5371ddaaf
Vivien <vivien-public@pm.me>
2025-06-11 22:11:24 -0400

    Play with Header component props & in-line styling

--- feedback-app/src/App.tsx
+++ feedback-app/src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-      <Header text="Header component" />
+      <Header text="Header component" bgColor="blue" textColor="white" />
       <div className="container">
         <h1>App component</h1>
       </div>
--- feedback-app/src/components/Header.tsx
+++ feedback-app/src/components/Header.tsx
@@ -1,10 +1,17 @@
 type HeaderProps = {
   text: string
+  bgColor: string
+  textColor: string
 }
 
-function Header({ text }: HeaderProps) {
+function Header({ text, bgColor, textColor }: HeaderProps) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
