7c522b2ea4750d7d7ef7cde0013514dcd54ae290
Vivien <vivien-public@pm.me>
2025-06-13 08:12:54 -0400

    Add About page and basic routing

--- feedback-app/src/App.tsx
+++ feedback-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router'
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
@@ -5,6 +6,8 @@ import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+// Import all routes
+import About from './pages/About'
 
 type FeedbackObj = {
   id: number
@@ -38,18 +41,28 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedbackData={feedbackData} />
-        {/* Have to pass handleDelete from here, since this is where the data is */}
-        <FeedbackList
-          feedbackData={feedbackData}
-          handleDelete={deleteFeedback}
-        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedbackData={feedbackData} />
+                {/* Have to pass handleDelete from here, since this is where the data is */}
+                <FeedbackList
+                  feedbackData={feedbackData}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
---
+++ feedback-app/src/pages/About.tsx
@@ -0,0 +1,18 @@
+import Card from '../components/shared/Card'
+
+function About() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React project to leave feedback for a service.</p>
+        <p>Version: 1.0.0</p>
+        <p>
+          <a href="/">Back to Home</a>
+        </p>
+      </div>
+    </Card>
+  )
+}
+
+export default About
