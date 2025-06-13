0e23cf4ead72bc21ef078e967397e550d9fc3d6b
Vivien <vivien-public@pm.me>
2025-06-13 07:23:01 -0400

    Add animation to feedback add/delete

--- feedback-app/src/components/FeedbackList.tsx
+++ feedback-app/src/components/FeedbackList.tsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'motion/react'
 import FeedbackItem from './FeedbackItem'
 
 type FeedbackDataItem = {
@@ -18,11 +19,32 @@ function FeedbackList({ feedbackData, handleDelete }: FeedbackListProps) {
 
   return (
     <div className="feedback-list">
-      {feedbackData.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedbackData.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedbackData.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 export default FeedbackList
