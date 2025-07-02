1fd7b7521561656b992bc3a050c484c41b970887
Vivien <vivien-public@pm.me>
2025-07-01 20:50:53 -0400

    Add useState to EmailInput component

--- projects/test-app/src/components/EmailInput.tsx
+++ projects/test-app/src/components/EmailInput.tsx
@@ -1,23 +1,23 @@
-import { type FocusEvent } from 'react'
+import { useState, type FocusEvent } from 'react'
 
 export function EmailInput() {
-  let errorMsg = ''
+  const [errorInfo, setErrorInfo] = useState('')
 
   // input validation
   function validateEmail(event: FocusEvent<HTMLInputElement>) {
     console.log('validating input')
     const enteredEmail = event.target.value
     if (enteredEmail.trim() === '' || !enteredEmail.includes('@')) {
-      errorMsg = 'The entered email address is invalid.'
+      setErrorInfo('The entered email address is invalid.')
     } else {
-      errorMsg = ''
+      setErrorInfo('')
     }
   }
 
   return (
     <div>
       <input placeholder="Your email" type="email" onBlur={validateEmail} />
-      <p>{errorMsg}</p>
+      <p>{errorInfo}</p>
     </div>
   )
 }
